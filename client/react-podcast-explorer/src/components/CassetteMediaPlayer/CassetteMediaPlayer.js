28f27ddc971c420c77343a897854b25b263a07eb
Rob Brennan <rob@therobbrennan.com>
2019-11-09 16:24:29 -0800

    Implemented CassetteMediaPlayer and styling

--- client/react-podcast-explorer/src/components/CassetteMediaPlayer/CassetteMediaPlayer.js
+++ client/react-podcast-explorer/src/components/CassetteMediaPlayer/CassetteMediaPlayer.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
+// Cassette Media Player - https://benwiley4000.github.io/cassette/styleguide/#cassette-quick-start
+import { MediaPlayer } from '@cassette/player';
+// import '@cassette/player/dist/css/cassette-player.css';
+import './CassetteMediaPlayer.css';
+
+// Custom playback control
+import PlaybackRateControl from './PlaybackRateControl';
+
+// TODO: Replace hard-coded playlist
+const playlist = [
+  {
+    url:
+      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    title: 'Big Buck Bunny',
+  },
+  {
+    url:
+      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+    title: 'Elephants Dream',
+  },
+];
+
 export default () => {
   return (
-    <p>CassetteMediaPlayer</p>
-  )
-}
+    <MediaPlayer
+      playlist={playlist}
+      showVideo
+      controls={[
+        'spacer',
+        'backskip',
+        'playpause',
+        'forwardskip',
+        'mute',
+        playerContext => (
+          <PlaybackRateControl
+            playbackRate={playerContext.playbackRate}
+            onSetPlaybackRate={playerContext.onSetPlaybackRate}
+          />
+        ),
+        'spacer',
+        'progress',
+      ]}
+    />
+  );
+};
