730c6c5e0199b30f1b24f1c263db4a8fd2a4bb93
Rob Brennan <rob@therobbrennan.com>
2019-11-09 16:56:13 -0800

    Incorporated crude playlist implementation

--- client/react-podcast-explorer/src/components/CassetteMediaPlayer/CassetteMediaPlayerAdvanced.js
+++ client/react-podcast-explorer/src/components/CassetteMediaPlayer/CassetteMediaPlayerAdvanced.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 // Cassette Media Player - https://benwiley4000.github.io/cassette/styleguide/#cassette-quick-start
-import { MediaPlayer } from '@cassette/player';
+import { MediaPlayerControls } from '@cassette/player';
+import { PlayerContextProvider } from '@cassette/core';
 import './CassetteMediaPlayer.css';
 
-// Custom playback control
+// Custom playback control and menu
 import PlaybackRateControl from './PlaybackRateControl';
+import PlaylistMenu from './PlaylistMenu';
 
 // TODO: Replace hard-coded playlist
 const playlist = [
@@ -23,24 +25,29 @@ const playlist = [
 
 export default () => {
   return (
-    <MediaPlayer
-      playlist={playlist}
-      showVideo
-      controls={[
-        'spacer',
-        'backskip',
-        'playpause',
-        'forwardskip',
-        'mute',
-        playerContext => (
-          <PlaybackRateControl
-            playbackRate={playerContext.playbackRate}
-            onSetPlaybackRate={playerContext.onSetPlaybackRate}
-          />
-        ),
-        'spacer',
-        'progress',
-      ]}
-    />
+    <PlayerContextProvider playlist={playlist}>
+      <MediaPlayerControls
+        showVideo
+        controls={[
+          'spacer',
+          'backskip',
+          'playpause',
+          'forwardskip',
+          'mute',
+          playerContext => (
+            <PlaybackRateControl
+              playbackRate={playerContext.playbackRate}
+              onSetPlaybackRate={playerContext.onSetPlaybackRate}
+            />
+          ),
+          'spacer',
+          'progress',
+        ]}
+      />
+      <div>
+        <h3>Select a track:</h3>
+        <PlaylistMenu />
+      </div>
+    </PlayerContextProvider>
   );
 };
